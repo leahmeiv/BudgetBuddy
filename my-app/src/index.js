cfd5f38f911ae8fa74e238378654d8ba4ed2d62d
Leah-Mei Villanueva <leahmei@hawaii.edu>
2020-02-21 20:27:24 -1000

    cancel

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,18 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
+import { Container, Header, Menu, Image, Grid, List} from 'semantic-ui-react';
 
+class TopMenu extends React.Component {
+  render() {
+    return (
+      <Menu borderless inverted className="topmenu">
+        <Container>
+          {/*<Menu.Item as={'a'} className="logo">*/}
+            {/*<Image src=""/>*/}
+          {/*</Menu.Item>*/}
+          <Menu.Item as={'a'} href={'home.html'} className="right item">Home</Menu.Item>
+          <Menu.Item as={'a'} href={'about.html'} >About</Menu.Item>
+          <Menu.Item as={'a'} href={'contact.html'}>Contact</Menu.Item>
+        </Container>
+      </Menu>
+    )
+  }
+}
 
 
 class BudgetBuddy extends React.Component {
-
   render() {
     return (
-      <h1>Budget Buddy!</h1>
+      <div>
+        <TopMenu/>
+      </div>
     );
   }
 }
 
 ReactDOM.render(<BudgetBuddy />, document.getElementById('root'));
-
-// export default BudgetBuddy
