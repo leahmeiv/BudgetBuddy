239aa04af21f599732c8634ba2f2076479c68b37
Leah-Mei Villanueva <leahmei@hawaii.edu>
2020-02-21 16:20:57 -1000

    commit react changes

---
+++ my-app/src/js/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+
+
+
+class BudgetBuddy extends React.Component {
+
+  render() {
+    return (
+      <h1>Budget Buddy!</h1>
+    );
+  }
+}
+
+ReactDOM.render(<BudgetBuddy />, document.getElementById('root'));
