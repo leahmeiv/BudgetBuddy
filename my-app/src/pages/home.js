9472bc5b55746c8fbd35f730f37b2cde1fb00b53
Leah-Mei Villanueva <leahmei@hawaii.edu>
2020-03-07 17:45:24 -1000

    edits

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 import '../css/home.css';
 import Logo from  '../images/menu-logo.png';
 import HomeBackground from '../images/home-background.jpeg';
+import GithubLogo from '../images/github-logo.png';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Dropdown, Menu, Image, Icon, Search, Button } from 'semantic-ui-react';
+import { Container, Dropdown, Menu, Image, Icon, Search, Button, List, Grid } from 'semantic-ui-react';
 
 
 class TopMenu extends React.Component {
@@ -87,6 +88,59 @@ class Background extends React.Component {
   }
 }
 
+class FooterMenu extends React.Component{
+  render(){
+    return(
+      <Container className="footer-background">
+        <Grid divided='vertically'>
+          <Grid.Row internally celled columns={3}>
+
+            <Grid.Column>
+              <b>Information</b>
+              <List link className="ui link list">
+                <List.Item as='a'>About Us</List.Item>
+                <List.Item as='a'>Message to Our Users</List.Item>
+              </List>
+            </Grid.Column>
+
+            <Grid.Column>
+              <b>Need Help?</b>
+              <List link className="ui link list">
+                <List.Item as='a'>Support</List.Item>
+
+              </List>
+            </Grid.Column>
+
+            <Grid.Column>
+              <b>Connect</b>
+              <List link className="ui link list">
+                <List.Item as='a'>Get Started</List.Item>
+                <List.Item as='a'>Account</List.Item>
+                <List.Item as='a'>Terms and Conditions</List.Item>
+                <List.Item as='a'>
+                  <Image src={GithubLogo}/>Github Repository</List.Item>
+              </List>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    )
+  }
+}
+
+class FooterMenuTwo extends React.Component{
+  render(){
+    return(
+      <Container className="ui bottom secondary menu">
+        <p className="item">
+          <Icon name="copyright outline"/>
+          Copyright 2020 BudgetBuddy Inc. All Rights Reserved | Legal Terms | Privacy Policy | Cookie Policy </p>
+
+      </Container>
+    )
+  }
+}
+
 
 
 class Home extends React.Component {
@@ -96,6 +150,8 @@ class Home extends React.Component {
       <div>
         <TopMenu/>
         <Background/>
+        <FooterMenu/>
+        <FooterMenuTwo/>
       </div>
     );
   }
