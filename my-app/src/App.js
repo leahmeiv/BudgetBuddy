98bf7a588b50b533aee261b6d54b2d42c66a3899
Leah-Mei Villanueva <leahmei@hawaii.edu>
2020-03-07 18:59:32 -1000

    convert about page to react

---
+++ my-app/src/App.js
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+// import 'semantic-ui-css/semantic.css';
+// import Home from './pages/home.js';
+// import About from './pages/about.js';
+//
+//
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+//
+//           <Switch>
+//             <Route exact path="/" component={Home}/>
+//             {/*<Route exact path="/userprofile" component={UserProfile}/>*/}
+//             {/*<Route path="/signin" component={Signin}/>*/}
+//             {/*<Route path="/signup" component={Signup}/>*/}
+//             <Route exact path="/home" component={Home}/>
+//             <Route path="/about" component={About}/>
+//             {/*<Route path="/createuserprofile" component={CreateUserProfile}/>*/}
+//
+//             {/*<ProtectedRoute path="/edituserprofile/:_id" component={EditUserProfile}/>*/}
+//             {/*<ProtectedRoute path="/userprofilebyid/:_id" component={UserProfileById}/>*/}
+//             {/*<AdminProtectedRoute path="/admin" component={AdminPage}/>*/}
+//             {/*<AdminProtectedRoute path="/edit-user/:_id" component={EditUserAdmin}/>*/}
+//             {/*<AdminProtectedRoute path="/edit-category/:_id" component={EditCategoryAdmin}/>*/}
+//             {/*<AdminProtectedRoute path="/edit-item/:_id" component={EditItemAdmin}/>*/}
+//             {/*<AdminProtectedRoute path="/edit-report/:_id" component={EditReportAdmin}/>*/}
+//             {/*<ProtectedRoute path="/signout" component={Signout}/>*/}
+//             {/*<Route component={NotFound}/>*/}
+//           </Switch>
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
+//
+// export default App;
