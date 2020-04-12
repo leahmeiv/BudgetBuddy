ab87253607787363b4d0aa526175474e994cdd4e
Levy Matsuda <levyjean@hawaii.edu>
2020-04-11 23:11:38 -1000

    import axios

--- my-app/src/pages/login.js
+++ my-app/src/pages/login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import '../css/about.css';
 import { Link, Redirect } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
@@ -19,7 +20,7 @@ class Login extends React.Component {
   }
 
   render() {
-  
+
     return (
       <div>
         <div>
