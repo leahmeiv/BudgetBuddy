939797aa8813107e424190226c090d580479c988
Levy Matsuda <levyjean@hawaii.edu>
2020-04-26 10:42:55 -1000

    update

--- app/client/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import ReactDOM from 'react-dom';
-import App from '../imports/ui/pages/App';
-
-Meteor.startup(() => {
-  ReactDOM.render(<App/>, document.getElementById('react-target'));
-});
