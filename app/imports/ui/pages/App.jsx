867d0620f9580456d15a9541bd5c1a6823ff4409
Levy Matsuda <levyjean@hawaii.edu>
2020-04-26 08:41:28 -1000

    updates

---
+++ app/imports/ui/pages/App.jsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import 'semantic-ui-css/semantic.css';
+import Home from './Home';
+//import About from './pages/about.js';
+import NavBar from '../components/Navbar';
+import Footer from '../components/Footer';
+//import Login from './pages/login.js';
+//import Signup from './pages/signup.js'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='Site'>
+          <NavBar/>
+          <div className='Site-content'>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+            </Switch>
+          </div>
+          <Footer/>
+        </div>
+      </Router>
+
+
+//           <Switch>
+//             {/*<Route exact path="/userprofile" component={UserProfile}/>*/}
+//             {/*<Route path="/signin" component={Signin}/>*/}
+//             {/*<Route path="/signup" component={Signup}/>*/}
+//             {/*<Route path="/createuserprofile" component={CreateUserProfile}/>*/}
+//
+//             {/*<ProtectedRoute path="/edituserprofile/:_id" component={EditUserProfile}/>*/}
+//             {/*<ProtectedRoute path="/userprofilebyid/:_id" component={UserProfileById}/>*/}
+//             {/*<AdminProtectedRoute path="/admin" component={AdminPage}/>*/}
+//             {/*<AdminProtectedRoute path="/edit-user/:_id" component={EditUserAdmin}/>*/}
+//             {/*<AdminProtectedRoute path="/edit-category/:_id" component={EditCategoryAdmin}/>*/}
+//             {/*<AdminProtectedRoute path="/edit-item/:_id" component={EditItemAdmin}/>*/}
+//             {/*<AdminProtectedRoute path="/edit-report/:_id" component={EditReportAdmin}/>*/}
+//             {/*<ProtectedRoute path="/signout" component={Signout}/>*/}
+//             {/*<Route component={NotFound}/>*/}
+//           </Switch>
+
+    );
+  }
+}
+
+export default App;
